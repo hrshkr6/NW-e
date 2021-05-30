5bfb039d8bfaa01f20987fdc31f16302a4d0a598
Anurag Harsh <hrshkr6@gmail.com>
2021-05-30 18:12:59 +0530

    web to

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          NauticalWings Website
+          NauticalWings Web 
         </a>
       </header>
     </div>
