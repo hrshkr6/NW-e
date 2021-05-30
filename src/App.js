e2d784b0bc6822bd0ae0514760d02b7b4e6915f1
NarayanAdi08 <aditya.jackson.08@gmail.comgit config --global user.name NarayanAdi08>
2021-05-30 18:54:55 +0600

    WebSite

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          NauticalWings WEB 
+          NauticalWings WebSite 
         </a>
       </header>
     </div>
